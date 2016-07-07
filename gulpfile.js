8dd2abbf3f9a60766fd03b30a0eff1b7664ffb14
charlieroberts <charlie@charlie-roberts.com>
2016-07-07 14:16:57 -0700

    adding gitignore and initial gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp'),
+    //uglify   = require('gulp-uglify'),
+    notify     = require('gulp-notify'),
+    babel      = require('gulp-babel'),
+    browserify = require('browserify'),
+    //tap       = require('gulp-tap'),
+    buffer     = require('gulp-buffer'),
+    source     = require('vinyl-source-stream'),
+    babelify   = require('babelify'),
+    mocha      = require('gulp-mocha')
+
+gulp.task( 'js', function() {
+  browserify({ debug:true, standalone:'genish' })
+    .transform( babelify, { presets:['es2015'] })
+    .require( './js/index.js', { entry: true } ) 
+    .bundle()
+    .pipe( source('gen.lib.js') )
+    .pipe( gulp.dest('./dist') )
+    //.pipe( uglify() )
+    //.pipe( gulp.dest('./dist') )
+    .pipe( 
+      notify({ 
+        message:'Build has been completed',
+        onLast:true
+      }) 
+    )
+  
+  // transpile (but don't browserify) for use with node.js tests
+  return gulp.src( './js/**.js' )
+    .pipe( babel({ presets:['es2015'] }) )
+    .pipe( gulp.dest('./dist' ) )
+
+})
+
+gulp.task( 'test', ['js'], ()=> {
+  return gulp.src('tests/gen.tests.js', {read:false})
+    .pipe( mocha({ reporter:'nyan' }) ) // spec, min, nyan, list
+})
+
+
+gulp.task( 'watch', function() {
+  gulp.watch( './js/**.js', ['test'] )
+})
+
+gulp.task( 'default', ['js','test'] )
